fbea21a23e5833f5d820ff3e5cf2e8b63c656c2d
ystalaya <ystalaya2009@gmail.com>
2020-11-01 15:43:57 +0300

    To do list

---
+++ script.js
@@ -0,0 +1,14 @@
+let header = document.getElementById("header");
+console.log("//header",header);
+//setTimeout(function(){
+//    
+//   header.style.visibility = "hidden";
+//},3000)
+setInterval(() => {
+    if (header.style.color = "red"){
+        header.style.color = "blue";
+    }
+    else {
+        header.style.color = "red" 
+    }
+}, 2000);
